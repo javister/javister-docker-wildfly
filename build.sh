73b175ffaebe63a26a13f307722b78f27fd652c1
Victor Verbitsky <vektor@krista.ru>
2017-11-22 12:52:59 +0300

    Скрипт настройки разделён на два, для обеспечения возможности более тонкого вклинивания в процесс настройки в образах-наследниках. Убраны дневные метки образов.

--- build.sh
+++ build.sh
@@ -36,7 +36,6 @@ EOF
 
     WILDFLY_VERSION=8.2.1.krista24
     WILDFLY_CLASSIFIER=WF82-SwitchYard
-    DATE=$(date +"%Y-%m-%d")
 
     IMAGE_TAG="javister-docker-docker.bintray.io/javister/javister-docker-wildfly"
     PROXY_ARGS="--build-arg http_proxy=${http_proxy} \
@@ -48,14 +47,12 @@ EOF
         --build-arg WILDFLY_CLASSIFIER=${WILDFLY_CLASSIFIER} \
         --tag ${IMAGE_TAG}:latest \
         --tag ${IMAGE_TAG}:${WILDFLY_VERSION}-${WILDFLY_CLASSIFIER} \
-        --tag ${IMAGE_TAG}:${WILDFLY_VERSION}-${WILDFLY_CLASSIFIER}-${DATE} \
         ${PROXY_ARGS} \
         $@ \
         .
 
     [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:latest
     [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:${WILDFLY_VERSION}-${WILDFLY_CLASSIFIER}
-    [ "${release}" == "release" ] && docker push ${IMAGE_TAG}:${WILDFLY_VERSION}-${WILDFLY_CLASSIFIER}-${DATE}
 }
 
 CURRENT_DIR=$(pwd)
--- files/etc/my_init.d/80_prepare_wildfly.sh
+++ files/etc/my_init.d/80_prepare_wildfly.sh
@@ -14,22 +14,6 @@ fi
 mkdir --parents /config/wildfly/deployments
 chown --recursive system:system /config/wildfly
 
-expandenv /app/wildfly/setup/setup.cli.template > /app/wildfly/setup/setup.cli
-
-exec setuser system ./bin/standalone.sh \
-    --admin-only \
-    -b=${HOSTIP} \
-    -bmanagement=${HOSTIP} \
-    -Djboss.server.config.dir=${WILDFLY_CONFIG_DIR} \
-    -Djboss.server.data.dir=${WILDFLY_DATA_DIR} \
-    -Djboss.server.log.dir=${WILDFLY_LOG_DIR} \
-    ${SERVER_PARAMS} &
-wait4tcp -w 200 ${HOSTIP} 9990
-WILDFLY_PID=`ps -ef | grep -e "\\[Standalone\\]" | awk '{print $2}'`
-jboss-cli --file=/app/wildfly/setup/setup.cli
-kill ${WILDFLY_PID}
-wait4tcp -w 200 -c ${HOSTIP} 9990
-
 exec setuser system /app/wildfly/bin/add-user.sh \
     --confirm-warning \
     --enable \
