123a90eb3ea5d083d9db7d357215891ac5ed7e67
Victor Verbitsky <vektor@krista.ru>
2018-04-28 16:23:29 +0300

    Support for the DEBUG and DEBUG_SUSPEND options

--- files/etc/my_init.d/90_setup_wildfly.sh
+++ files/etc/my_init.d/90_setup_wildfly.sh
@@ -17,3 +17,9 @@ WILDFLY_PID=`ps -ef | grep -e "\\[Standalone\\]" | awk '{print $2}'`
 jboss-cli --file=/app/wildfly/setup/setup.cli
 kill ${WILDFLY_PID}
 wait4tcp -w 200 -c ${HOSTIP} 9990
+
+if [ "${DEBUG}" == "yes" ]; then
+    JVM_OPTS="${JVM_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y"
+    [ "${DEBUG_SUSPEND}" == "yes" ] && JVM_OPTS="${JVM_OPTS},suspend=y " || JVM_OPTS="${JVM_OPTS},suspend=n "
+fi
+echo "${JVM_OPTS}" > /etc/container_environment/JVM_OPTS
