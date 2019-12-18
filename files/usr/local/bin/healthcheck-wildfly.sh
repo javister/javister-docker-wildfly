9f0c1009f97f8f8476d60f47c05effb156f252e2
Victor Verbitsky <vektor@krista.ru>
2019-12-18 18:48:03 +0300

    Adding the RAR deployment type

--- files/usr/local/bin/healthcheck-wildfly.sh
+++ files/usr/local/bin/healthcheck-wildfly.sh
@@ -3,7 +3,7 @@
 cd /config/wildfly/deployments
 
 while true; do
-    deployments=$(ls -1 *.ear *.war *.jar 2> /dev/null | wc -l)
+    deployments=$(ls -1 *.ear *.war *.jar *.rar 2> /dev/null | wc -l)
     deploying=$(ls -1 *.isdeploying 2> /dev/null | wc -l)
     deployed=$(ls -1 *.deployed 2> /dev/null | wc -l)
     failed=$(ls -1 *.failed 2> /dev/null | wc -l)
