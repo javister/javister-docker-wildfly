13618a456fc1326d804dcec73c3e6f8cb7582cc9
Victor Verbitsky <vektor@krista.ru>
2019-12-23 15:48:30 +0300

    Fix the activaion of Debug mode

--- files/etc/my_init.d/90_setup_wildfly.sh
+++ files/etc/my_init.d/90_setup_wildfly.sh
@@ -18,8 +18,8 @@ jboss-cli --file=/app/wildfly/setup/setup.cli
 kill ${WILDFLY_PID}
 wait4tcp -w 200 -c ${HOSTIP} 9990
 
-if [ "${DEBUG}" == "yes" ]; then
+if [ "${DEBUG}" == "on" ]; then
     JVM_OPTS="${JVM_OPTS} -Xdebug -Xrunjdwp:transport=dt_socket,address=8787,server=y"
-    [ "${DEBUG_SUSPEND}" == "yes" ] && JVM_OPTS="${JVM_OPTS},suspend=y " || JVM_OPTS="${JVM_OPTS},suspend=n "
+    [ "${DEBUG_SUSPEND}" == "on" ] && JVM_OPTS="${JVM_OPTS},suspend=y " || JVM_OPTS="${JVM_OPTS},suspend=n "
 fi
 echo "${JVM_OPTS}" > /etc/container_environment/JVM_OPTS
