36b9f3f0ecae86fd6888a4f7b5f9e78a0ac4c96c
Yury Krupin <yu.krupin@krista.ru>
2021-12-01 20:40:14 +0300

    #223602 Включил образ с Wildfly 17 в сборку

--- testing/src/main/java/com/github/javister/docker/testing/wildfly/JavisterWildflyContainer.java
+++ testing/src/main/java/com/github/javister/docker/testing/wildfly/JavisterWildflyContainer.java
@@ -201,7 +201,8 @@ public class JavisterWildflyContainer<SELF extends JavisterWildflyContainer<SELF
     public enum Variant {
         WILDFLY8("8"),
         WILDFLY8_SWITCHYARD("8-switchyard"),
-        WILDFLY8_SWITCHYARD_MODESHAPE("8-switchyard-modeshape");
+        WILDFLY8_SWITCHYARD_MODESHAPE("8-switchyard-modeshape"),
+        WILDFLY17("17");
 
         private final String value;
 
